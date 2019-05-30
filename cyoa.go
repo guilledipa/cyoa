185e5090e3f46e589fa2008ceddb526885a44d4c
guilledipa <guilledipasquale@gmail.com>
2019-05-31 06:57:39 +1000

    Initial commit

---
+++ cyoa.go
@@ -0,0 +1,12 @@
+// Program recreates the "Choose your own adventure" experience via a web
+// application where each page will be a portion of the story, and at the end
+// of every page the user will be given a series of options to choose from (or
+// be told that they have reached the end of that particular story arc).
+// https://github.com/gophercises/cyoa
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Initial commit")
+}
